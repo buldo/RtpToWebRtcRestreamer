9e58e104934092d0f3aad7ff439d7a0125a998fa
Roman Buldygin <froooks@gmail.com>
2023-02-25 07:43:42 +0300

    Removing unused

--- src/Bld.RtpToWebRtcRestreamer/SIPSorcery/Net/RTP/VideoStream.cs
+++ src/Bld.RtpToWebRtcRestreamer/SIPSorcery/Net/RTP/VideoStream.cs
@@ -14,39 +14,18 @@
 // BSD 3-Clause "New" or "Revised" License, see included LICENSE.md file.
 //-----------------------------------------------------------------------------
 
-using System.Net;
-using System.Net.Sockets;
 using Bld.RtpToWebRtcRestreamer.Common;
-using Bld.RtpToWebRtcRestreamer.SIPSorcery.Net.RTP.Packetisation;
 using Bld.RtpToWebRtcRestreamer.SIPSorcery.Net.SDP;
-using Bld.RtpToWebRtcRestreamer.SIPSorcery.Sys;
-using Microsoft.Extensions.Logging;
 using SIPSorceryMedia.Abstractions;
 
 namespace Bld.RtpToWebRtcRestreamer.SIPSorcery.Net.RTP
 {
     internal class VideoStream : MediaStream
     {
-        private static readonly ILogger Logger = Log.Logger;
-
-        private RtpVideoFramer _rtpVideoFramer;
-
         /// <summary>
         /// Gets fired when the remote SDP is received and the set of common video formats is set.
         /// </summary>
         public event Action<int, List<VideoFormat>> OnVideoFormatsNegotiatedByIndex;
-
-        /// <summary>
-        /// Gets fired when a full video frame is reconstructed from one or more RTP packets
-        /// received from the remote party.
-        /// </summary>
-        /// <remarks>
-        ///  - Received from end point,
-        ///  - The frame timestamp,
-        ///  - The encoded video frame payload.
-        ///  - The video format of the encoded frame.
-        /// </remarks>
-        public event Action<int, IPEndPoint, uint, byte[], VideoFormat> OnVideoFrameReceivedByIndex;
 
         /// <summary>
         /// Indicates whether this session is using video.
@@ -58,12 +37,6 @@ namespace Bld.RtpToWebRtcRestreamer.SIPSorcery.Net.RTP
                 return LocalTrack != null && LocalTrack.StreamStatus != MediaStreamStatusEnum.Inactive;
             }
         }
-
-        /// <summary>
-        /// Indicates the maximum frame size that can be reconstructed from RTP packets during the depacketisation
-        /// process.
-        /// </summary>
-        private int MaxReconstructedVideoFrameSize { get; } = 1048576;
 
         private void SendH264Frame(RTPPacket packet)
         {
